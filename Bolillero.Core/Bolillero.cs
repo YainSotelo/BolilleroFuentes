2c640089594c0fbb5dddb57d7dfb93250c58c95c
YainSotelo <yainsotelo3@gmail.com>
2021-08-11 15:27:00 -0300

    Update Bolillero.cs

--- Bolillero.Core/Bolillero.cs
+++ Bolillero.Core/Bolillero.cs
@@ -1,8 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Bolillero.Core
 {
     public class Bolillero
     {
+         byte cantidad {get; set;}
+
+        byte BolitasAzar{get; set;}
+           
+        List<byte>  afuera {get; set;}
+
+        List<byte> adentro {get; set;}
+
+        Random r;
+
+        public Bolillero()
+        {
+            afuera = new List<byte>();
+            adentro = new List<byte>();
+             r = new Random();
+
+        }
+        public Bolillero(byte cantidad)
+        {
+            byte.Lleno= new List<byte>();
+        }
+        private void Lleno(byte cantidad);{
+
+            for (byte i = 0; i < cantidad; i++) 
+            return bolillas
+        }
+
+      
+
+        
+        
+            
+
+
+
+
     }
-}
+
+    
