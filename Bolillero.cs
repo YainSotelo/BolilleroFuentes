3f9ff6c26b23b05dcc7cb858fa7cbec30efd89af
YainSotelo <yainsotelo3@gmail.com>
2021-08-25 15:54:05 -0300

    Create Bolillero.cs

---
+++ Bolillero.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+namespace Bolillero
+{
+    public class Bolillero
+    
+    {
+     List <byte> afuera {get;set;}  
+     List <byte> adentro {get;set;}
+
+     public cantidadAdentro 
+
+     public cantidadAfuera 
+    }
+}
+
+
+
+
+
+
+
+
+
