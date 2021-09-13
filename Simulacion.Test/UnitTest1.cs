3063d510f35635ec23e0fac629cf07eaf538df87
YainSotelo <yainsotelo3@gmail.com>
2021-09-13 16:18:46 -0300

    Update

--- Simulacion.Test/UnitTest1.cs
+++ Simulacion.Test/UnitTest1.cs
@@ -3,11 +3,28 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Simulacion.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class SimulacionTest
     {
+        Simulacion Simulacion{get;set;}
         [TestMethod]
-        public void TestMethod1()
+        public void SimulacionConTask()
         {
+            var Bolillero = new Bolillero.Core.Bolillero(10);
+
+            Simulacion = new Simulacion();
+
+            var jugada = new List<byte>{5};
+
+            long cantidadVeces = 10000000;
+
+            double esperado = cantidadVeces/10.0;
+
+            var ganadasSinHilos = Simulacion.SimularSinHilos(Bolillero, cantidadVeces, jugada);
+
+            var ganadasConHilos = Simulacion.SimularConHilos(Bolillero , cantidadVeces , jugada ,8);
+
+            assert.AreEqual(ganadasConHilos/esperado, ganadasSinHilos/esperado,0.1);
+
         }
     }
 }
